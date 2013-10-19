7103049b4c6c04e2e75c1f5af5cc5dd2f4be7e3d
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2013-10-19 23:54:13 +0000

    Change in package.js

--- package.js
+++ package.js
@@ -4,7 +4,10 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use('handlebars', 'client');
-  api.use('accounts-base', ['client', 'server']);
+  api.use([
+    'accounts-base',
+    'zeitgeist-model'
+  ], ['client', 'server']);
   
   api.add_files('lib/client/helpers.js', 'client');
   api.add_files('lib/voteable.js', ['client', 'server']);
