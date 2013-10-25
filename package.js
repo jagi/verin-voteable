0d96eb3d7f5c9eb5952beaf96adf1a123a7f0e03
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2013-10-26 00:41:41 +0200

    Refactoring

--- package.js
+++ package.js
@@ -11,6 +11,4 @@ Package.on_use(function (api) {
   
   api.add_files('lib/client/helpers.js', 'client');
   api.add_files('lib/voteable.js', ['client', 'server']);
-  
-  api.export('Voteable');
 });
